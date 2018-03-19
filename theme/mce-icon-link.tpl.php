0a2c8fb084200d701a1da8ace54e2210c7963abd
Junaid PV <junaid@detlus.com>
2018-03-20 00:37:45 +0530

    Allow overriding icon position and load style within CKEditor. Improve displaying of icon position.

--- theme/mce-icon-link.tpl.php
+++ theme/mce-icon-link.tpl.php
@@ -1,11 +1,11 @@
 <div class="<?php print $classes ?>"<?php print $attributes; ?>>
   <a href="<?php print $link_url; ?>">
-   <?php if ($settings['icon_position'] == 'left' || $settings['icon_position'] == 'top'): ?>
-    <span class="icon"><?php print render($icon); ?></span>
+   <?php if ($icon_position == 'right' || $icon_position == 'bottom'): ?>
     <span class="text"><?php print $link_text; ?></span>
+    <span class="icon"><?php print render($icon); ?></span>
    <?php else: ?>
-    <span class="text"><?php print $link_text; ?></span>
     <span class="icon"><?php print render($icon); ?></span>
+    <span class="text"><?php print $link_text; ?></span>
    <?php endif; ?>
   </a>
 </div>
