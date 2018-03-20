b508b308a4206ee7c6851c2bcea0a89e4095a85d
Junaid PV <junaid@detlus.com>
2018-03-20 22:59:41 +0530

    Print icon only if available.

--- theme/mce-icon-link.tpl.php
+++ theme/mce-icon-link.tpl.php
@@ -2,9 +2,9 @@
   <a href="<?php print $link_url; ?>">
    <?php if ($icon_position == 'right' || $icon_position == 'bottom'): ?>
     <span class="text"><?php print $link_text; ?></span>
-    <span class="icon"><?php print render($icon); ?></span>
+    <?php if ($icon): ?><span class="icon"><?php print render($icon); ?></span><?php endif; ?>
    <?php else: ?>
-    <span class="icon"><?php print render($icon); ?></span>
+    <?php if ($icon): ?><span class="icon"><?php print render($icon); ?></span><?php endif; ?>
     <span class="text"><?php print $link_text; ?></span>
    <?php endif; ?>
   </a>
