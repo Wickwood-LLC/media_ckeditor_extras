79a6c79ab1266d252e516d02d16dcdf4946c8932
Junaid PV <junu.pv@gmail.com>
2021-08-04 15:50:43 +0530

    Changes suggested by the Rector plugin to make compatible with D9.

--- theme/mce-icon-link.tpl.php
+++ theme/mce-icon-link.tpl.php
@@ -1,11 +1,22 @@
-<div class="<?php print $classes ?>"<?php print $attributes; ?>>
-  <a href="<?php print $link_url; ?>">
-   <?php if ($icon_position == 'right' || $icon_position == 'bottom'): ?>
+<?php
+
+?>
+<div class="<?php 
+print $classes ?>
+?>"<?php 
+print $attributes;
+?>>
+  <a href="<?php 
+print $link_url;
+?>">
+   <?php 
+if ($icon_position == 'right' || $icon_position == 'bottom'): ?>
     <span class="text"><?php print $link_text; ?></span>
     <?php if ($icon): ?><span class="icon"><?php print render($icon); ?></span><?php endif; ?>
    <?php else: ?>
     <?php if ($icon): ?><span class="icon"><?php print render($icon); ?></span><?php endif; ?>
     <span class="text"><?php print $link_text; ?></span>
-   <?php endif; ?>
+   <?php endif;
+?>
   </a>
-</div>
+</div><?php 
